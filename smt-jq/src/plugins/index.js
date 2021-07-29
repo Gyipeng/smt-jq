7e9fc8d054205218f013adecb0ccc21dd719dac9
gyp“git config --global user.name gyp“ <guoyipeng@feinno.com>
2021-07-29 14:59:11 +0800

    fix:jq

--- smt-jq/src/plugins/index.js
+++ smt-jq/src/plugins/index.js
@@ -1,8 +1,10 @@
-;(function (undefined) {
-  "use strict"
-  var _global;
+class SmtJq {
+  constructor(opt) {
+    this.init(opt)
+    this.render()
+  }
 
-  function extend(o, n, override) {
+  extend(o, n, override) {
     for (var key in n) {
       if (n.hasOwnProperty(key) && (!o.hasOwnProperty(key) || override)) {
         o[key] = n[key];
@@ -11,56 +13,39 @@
     return o;
   }
 
-  function SmtJq(opt) {
-    this._init(opt);
-  }
-
-  SmtJq.prototype = {
-    constructor: this,
-    _init: function (opt) {
-      // 默认参数
-      var def = {
-        dom: document.querySelector("#smt-jq"),
-        option: {
-          title: {
-            text: '第一个 ECharts 实例'
-          },
-          tooltip: {},
-          legend: {
-            data: ['销量']
-          },
-          xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-          },
-          yAxis: {},
-          series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }]
+  init(opt) {
+    let {className}=opt
+    let def = {
+      dom: document.querySelector(className),
+      option: {
+        title: {
+          text: '第一个 ECharts 实例'
         },
-      };
-
-      this.def = extend(def, opt, true); //配置参数
-      this.dom = this.def.dom
-      this.option = this.def.option
-      this.chart = echarts.init(this.dom)
-    },
-    render() {
-      this.chart.setOption(this.option);
-    }
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      },
+    };
+    this.def = this.extend(def, opt, true); //配置参数
+    this.dom = this.def.dom
+    this.option = this.def.option
+    this.chart = echarts.init(this.dom)
   }
 
-  _global = (function () {
-    return this || (0, eval)('this');
-  }());
-  if (typeof module !== "undefined" && module.exports) {
-    module.exports = SmtJq;
-  } else if (typeof define === "function" && define.amd) {
-    define(function () {
-      return SmtJq;
-    });
-  } else {
-    !('SmtJq' in _global) && (_global.SmtJq = SmtJq);
+  render() {
+    this.chart.setOption(this.option);
   }
-}());
+}
+
+export default SmtJq
+
